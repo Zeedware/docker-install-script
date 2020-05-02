aa79948d82784b7824238ff126bd5882c1174146
Kharriz Abiyasa <kharizet@gmail.com>
2020-05-02 16:20:47 +0700

    add post install

--- install-docker.sh
+++ install-docker.sh
@@ -7,3 +7,7 @@ sudo add-apt-repository \
    stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# post install
+sudo groupadd docker
+sudo usermod -aG docker $USER
